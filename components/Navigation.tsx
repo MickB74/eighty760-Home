de83d6a39f8d0219a860d52f45a8de21c797ee68
MickB74 <mickeybarry@gmail.com>
2026-01-10 21:16:30 -0500

    Revert "Use uploaded_image.png for logo to remove background"
    This reverts commit e1f666d1d7652e6c10e182fef90e3a78056ffad2.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -71,7 +71,7 @@ export default function Navigation() {
                     <div className="flex flex-row items-center gap-4">
                         <Link href="/" className="flex items-center">
                             <Image
-                                src="/uploaded_image.png"
+                                src="/logo.png"
                                 alt="Eighty760 Logo"
                                 width={240}
                                 height={120}
